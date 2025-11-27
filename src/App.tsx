f846a73243048ac7e96c12e07d92e834f711306b
Ivan Danilenkov <danilich071@gmail.com>
2025-11-27 00:23:35 -0500

    project: выполнена реализация всех основных функций

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { FC, CSSProperties, useState } from 'react';
+import clsx from 'clsx';
+
+import { Article } from './components/article/';
+import { ArticleParamsForm } from './components/article-params-form';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
+
+import './styles/index.scss';
+import styles from './styles/index.module.scss';
+
+type ArticleState = typeof defaultArticleState;
+
+export const App: FC = () => {
+	// открыт ли сайдбар
+	const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+	// состояние статьи
+	const [articleState, setArticleState] =
+		useState<ArticleState>(defaultArticleState);
+
+	// Состояние сайдбара (открыть/закрыть)
+	const handleToggleSidebar = () => {
+		setSidebarOpen((prev) => !prev);
+	};
+
+	// применить настройки
+	const handleApply = (newValues: ArticleStateType) => {
+		setArticleState(newValues);
+		setSidebarOpen(false);
+	};
+
+	// сброс к дефолтным значениям
+	const handleReset = () => {
+		setArticleState(defaultArticleState);
+	};
+
+	return (
+		<main
+			className={clsx(styles.main)}
+			style={
+				{
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
+				} as CSSProperties
+			}>
+			{isSidebarOpen && (
+				<div className={styles.overlay} onClick={handleToggleSidebar}></div>
+			)}
+			<ArticleParamsForm
+				isOpen={isSidebarOpen}
+				onToggle={handleToggleSidebar}
+				onApply={handleApply}
+				onReset={handleReset}
+				initialValues={articleState}
+			/>
+			<Article />
+		</main>
+	);
+};
